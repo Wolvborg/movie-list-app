8c663ae2ca431227811912cb4ee675938805d067
Wolvborg <binnu24668@gmail.com>
2023-03-17 19:25:37 +0530

    modification in controllers

--- src/main/java/org/wolb/movies/controller/MovieController.java
+++ src/main/java/org/wolb/movies/controller/MovieController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.wolb.movies.exception.InvalidEntryException;
 import org.wolb.movies.model.Movie;
 import org.wolb.movies.service.MovieService;
 
@@ -18,18 +20,27 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
-    @GetMapping({"","/", "/all"})
-    public ResponseEntity<List<Movie>> getAllMovies(){
+    @GetMapping({ "", "/", "/all" })
+    public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
     }
 
+    @GetMapping("/search")
+    public String getMovieByIdParam(@RequestParam String id,
+            @RequestParam(value = "p") String page) {
+        return id + page;
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String id){
-        return new ResponseEntity<>(movieService.movieById(id), HttpStatus.OK);
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String id) {
+        if (id.matches("-?\\d+"))
+            return new ResponseEntity<>(movieService.movieById(id), HttpStatus.OK);
+        else
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The ID contained a non-numerical value.");
     }
 
     @PostMapping("/add")
-    public  ResponseEntity<String> addNewMovie(@RequestBody Map<String, String> payload){
+    public ResponseEntity<String> addNewMovie(@RequestBody Map<String, String> payload) {
         String title = payload.get("title");
         String imdbId = payload.get("imdbId");
         String releaseDate = payload.get("releaseDate");
@@ -39,6 +50,11 @@ public class MovieController {
         movieService.addMovie(new Movie(imdbId, title, releaseDate, trailerLink, poster));
 
         return new ResponseEntity<>("Movie " + title + " added", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/testException")
+    public void testInvalidEntryException() {
+        throw new InvalidEntryException();
     }
 
 }
